53978fa42978a4eeb61bdde1ed7b76001f7423d2
Toufik Zitouni <toufik_z@hotmail.com>
2019-07-14 23:40:02 -0700

    Make content of bidirectional scroll more efficient and smoother when scrolling
    Added slivers support to make large content smoother on scroll. I have
    tried to add calculating the height and width if not provided however
    it would be more efficient if they’re both passed as params if known.
    Otherwise, it will try to do its best to figure the width and height

--- lib/bidirectional_scroll_view.dart
+++ lib/bidirectional_scroll_view.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class BidirectionalScrollViewPlugin extends StatefulWidget {
-  BidirectionalScrollViewPlugin({
-    @required this.child,
+  BidirectionalScrollViewPlugin({@required this.child,
+    this.childWidth,
+    this.childHeight,
     this.velocityFactor,
+    this.initialOffset,
+    this.scrollDirection,
     this.scrollListener,
     this.scrollOverflow = Overflow.visible,
   });
 
   final Widget child;
+  final double childWidth;
+  final double childHeight;
   final double velocityFactor;
+  final Offset initialOffset;
+  final ScrollDirection scrollDirection;
   final ValueChanged<Offset> scrollListener;
   final Overflow scrollOverflow;
 
@@ -18,9 +26,21 @@ class BidirectionalScrollViewPlugin extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    _state = new _BidirectionalScrollViewState(
-        child, velocityFactor, scrollListener);
+    if (_state == null) {
+      _state = new _BidirectionalScrollViewState(
+          child,
+          childWidth,
+          childHeight,
+          velocityFactor,
+          initialOffset,
+          scrollDirection,
+          scrollListener);
+    }
     return _state;
+  }
+
+  set initialOffset(Offset offset) {
+    _state.initOffset = offset;
   }
 
   // set x and y scroll offset of the overflowed widget
@@ -63,9 +83,14 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
     with SingleTickerProviderStateMixin {
   final GlobalKey _containerKey = new GlobalKey();
   final GlobalKey _positionedKey = new GlobalKey();
+  final GlobalKey _childKey = new GlobalKey();
 
   Widget _child;
+  double _childWidth;
+  double _childHeight;
   double _velocityFactor = 1.0;
+  Offset _initialOffset = new Offset(0.0, 0.0);
+  ScrollDirection _scrollDirection = ScrollDirection.both;
   ValueChanged<Offset> _scrollListener;
 
   double xPos = 0.0;
@@ -78,22 +103,49 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
 
   bool _enableFling = false;
 
-  _BidirectionalScrollViewState(Widget child, double velocityFactor,
+  _BidirectionalScrollViewState(Widget child, double childWidth,
+      double childHeight, double velocityFactor,
+      Offset initialOffset, ScrollDirection scrollDirection,
       ValueChanged<Offset> scrollListener) {
     _child = child;
+    _childWidth = childWidth;
+    _childHeight = childHeight;
     if (velocityFactor != null) {
       this._velocityFactor = velocityFactor;
     }
     if (scrollListener != null) {
       _scrollListener = scrollListener;
     }
+    if (scrollDirection != null) {
+      _scrollDirection = scrollDirection;
+    }
+    if (initialOffset != null) {
+      _initialOffset = initialOffset;
+      xViewPos = _initialOffset.dx;
+      yViewPos = _initialOffset.dy;
+    }
   }
 
   @override
   void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
     super.initState();
     _controller = new AnimationController(vsync: this)
       ..addListener(_handleFlingAnimation);
+  }
+
+  _afterLayout(_) {
+    final RenderBox renderBox = _childKey.currentContext.findRenderObject();
+    setState(() {
+      _childWidth = renderBox.size.width;
+      _childHeight = renderBox.size.height;
+    });
+  }
+
+  set initOffset(Offset offset) {
+    setState(() {
+      _initialOffset = offset;
+    });
   }
 
   set offset(Offset offset) {
@@ -132,8 +184,7 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
   }
 
   void _handleFlingAnimation() {
-    if (!_enableFling ||
-        _flingAnimation.value.dx.isNaN ||
+    if (!_enableFling || _flingAnimation.value.dx.isNaN ||
         _flingAnimation.value.dy.isNaN) {
       return;
     }
@@ -141,14 +192,14 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
     double newXPosition = xPos + _flingAnimation.value.dx;
     double newYPosition = yPos + _flingAnimation.value.dy;
 
-    if (newXPosition > 0.0 || width < containerWidth) {
-      newXPosition = 0.0;
+    if (newXPosition > _initialOffset.dx || width < containerWidth) {
+      newXPosition = _initialOffset.dx;
     } else if (-newXPosition + containerWidth > width) {
       newXPosition = containerWidth - width;
     }
 
-    if (newYPosition > 0.0 || height < containerHeight) {
-      newYPosition = 0.0;
+    if (newYPosition > _initialOffset.dy || height < containerHeight) {
+      newYPosition = _initialOffset.dy;
     } else if (-newYPosition + containerHeight > height) {
       newYPosition = containerHeight - height;
     }
@@ -172,14 +223,15 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
     double containerWidth = containerBox.size.width;
     double containerHeight = containerBox.size.height;
 
-    if (newXPosition > 0.0 || width < containerWidth) {
-      newXPosition = 0.0;
+
+    if (newXPosition > _initialOffset.dx || width < containerWidth) {
+      newXPosition = _initialOffset.dx;
     } else if (-newXPosition + containerWidth > width) {
       newXPosition = containerWidth - width;
     }
 
-    if (newYPosition > 0.0 || height < containerHeight) {
-      newYPosition = 0.0;
+    if (newYPosition > _initialOffset.dy || height < containerHeight) {
+      newYPosition = _initialOffset.dy;
     } else if (-newYPosition + containerHeight > height) {
       newYPosition = containerHeight - height;
     }
@@ -216,9 +268,9 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
 
     _enableFling = true;
     _flingAnimation = new Tween<Offset>(
-            begin: new Offset(0.0, 0.0),
-            end: direction * distance * _velocityFactor)
-        .animate(_controller);
+        begin: new Offset(0.0, 0.0),
+        end: direction * distance * _velocityFactor
+    ).animate(_controller);
     _controller
       ..value = 0.0
       ..fling(velocity: velocity);
@@ -232,23 +284,67 @@ class _BidirectionalScrollViewState extends State<BidirectionalScrollViewPlugin>
 
   @override
   Widget build(BuildContext context) {
+    if (_scrollDirection == ScrollDirection.horizontal) {
+      yViewPos = _initialOffset.dy;
+    }
+
+    if (_scrollDirection == ScrollDirection.vertical) {
+      xViewPos = _initialOffset.dx;
+    }
+
+    if (_childWidth == null && _childHeight == null) {
+      // This is just a workaround to get the width and height of child widget
+      return new Stack(
+        overflow: Overflow.visible,
+        children: <Widget>[
+          new Positioned(
+              top: 0,
+              left: 0,
+              child: new Container(
+                key: _childKey,
+                child: _child,
+              )
+          )
+        ],
+      );
+    }
+
     return new GestureDetector(
       onPanDown: _handlePanDown,
       onPanUpdate: _handlePanUpdate,
       onPanEnd: _handlePanEnd,
       child: new Container(
-        key: _containerKey,
-        child: new Stack(
-          overflow: widget.scrollOverflow,
-          children: <Widget>[
-            new Positioned(
+          key: _containerKey,
+          color: Colors.transparent,
+          child: new Stack(
+            overflow: widget.scrollOverflow,
+            children: <Widget>[
+              new Positioned(
                 key: _positionedKey,
                 top: yViewPos,
                 left: xViewPos,
-                child: _child),
-          ],
-        ),
+                width: _childWidth,
+                height: _childHeight,
+                child: new CustomScrollView(
+                    physics: new NeverScrollableScrollPhysics(),
+                    slivers: [
+                      SliverSafeArea(
+                        sliver: SliverFillRemaining(
+                          child: _child,
+                        ),
+                      )
+                    ]
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
+}
+
+enum ScrollDirection {
+  horizontal,
+  vertical,
+  both
 }
